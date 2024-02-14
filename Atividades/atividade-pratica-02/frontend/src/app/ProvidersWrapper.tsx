45269e6ec5e21457aa2cd4eea2281865a7192a57
Filipe <filipeasm18@gmail.com>
2024-02-14 18:31:52 -0300

    Fix: Removed devtoolts

--- Atividades/atividade-pratica-02/frontend/src/app/ProvidersWrapper.tsx
+++ Atividades/atividade-pratica-02/frontend/src/app/ProvidersWrapper.tsx
@@ -1,28 +1,23 @@
-"use client";
+"use client"
 
-
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactNode } from "react"
 
 interface ProvidersWrapperProps {
-  children: ReactNode;
+  children: ReactNode
 }
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       refetchOnWindowFocus: false,
-      refetchInterval: 120000, // 2 minutes
-    },
-  },
-});
+      refetchInterval: 120000 // 2 minutes
+    }
+  }
+})
 
 export default function ProvidersWrapper({ children }: ProvidersWrapperProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools />
-        {children}
-    </QueryClientProvider>
-  );
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  )
 }
